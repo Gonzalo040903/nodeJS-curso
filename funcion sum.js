bffd7f3c28ffcc92875139de234e99189a8d5338
Gonzalo <gonzalo040903@gmail.com>
2024-04-08 22:04:18 -0300

    Feature: añade funcion export import

---
+++ funcion sum.js
@@ -0,0 +1,7 @@
+//funcion suma
+function sum(a, b) {
+    return a + b;
+};
+
+//exporta la funcion
+module.exports = sum
---
+++ index.js
@@ -0,0 +1,5 @@
+//trae la funcion exportada de funcion sum.js
+const { sum } = require('./funcion sum')
+
+//muestra en consola
+console.log(sum(3, 10));
